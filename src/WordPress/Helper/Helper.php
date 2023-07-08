74b40080a42d8419bc1122c63d16d009f58a1c32
Agung Sundoro <muhagung2001@gmail.com>
2023-07-08 16:36:20 +0700

    feat: add support WP debug function

--- src/WordPress/Helper/Helper.php
+++ src/WordPress/Helper/Helper.php
@@ -23,4 +23,17 @@ class Helper
 	use Template;
 	use Validate;
 	use User;
+
+	/**
+	 * WordPress debug function.
+	 * - Please don't forget to turn on WP_DEBUG_LOG to true otherwise this function will not work.
+	 */
+	public function debug($log)
+	{
+		if (is_array($log) || is_object($log)) {
+			error_log(print_r($log, true));
+		} else {
+			error_log($log);
+		}
+	}
 }
