06e4a2c38345b02d2b34ab81119a6fb7f823d50a
Agung Sundoro <muhagung2001@gmail.com>
2023-07-08 16:30:53 +0700

    fix: gruntfile build no ts config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 /** Load Javascript Library */
 const path = require('path')
 let componentConfig = require( path.resolve(__dirname, 'assets/components/components.json') );
+let scriptConfig = require( path.resolve(__dirname, 'assets/ts/scripts.json') );
 let blockConfig = require( path.resolve(__dirname, 'blocks/blocks.json') );
 
 /** Export Module */
@@ -11,24 +12,39 @@ module.exports = function (grunt) {
 
 		/** Compile TailwindCSS - Cross Platform */
 		shell: {
-			...(() => { /** Get Shortcode Build Commands */
-				let shell = {}
+			...(() => { /** Get Svelte Build Components */
+			let shell = {};
+				componentConfig.map((c) => {
+					shell[`rollup_component_${c.name}`] = {
+						command: `npx rollup -c assets/components/${c.name}/rollup.config.js`
+					};
+				});
+				return shell;
+			})(),
+			...(() => { /** Get Typescript Library */
+			let shell = {};
+				scriptConfig.map((s) => {
+					shell[`npm_webpack_${s.name}`] = {
+						command: `npx webpack --config-name ${s.name}`
+					}
+				});
+				return shell;
+			})(),
+			...(() => { /** Get Blocks Build Commands */
+			let shell = {}
 				blockConfig.map((b) => {
-					shell[`build_shortcode_${b.name}`] = {
+					shell[`build_block_${b.name}`] = {
 						command: `npx vite build -c blocks/${b.name}/vite.config.js`
 					}
 				})
 				return shell;
 			})(),
-			dot_refactor: {
-				command: `aspri --wp-refactor --path ${path.resolve( __dirname )} --from Dot --to Dot --type theme && composer dump-autoload`,
-			},
 			npm_tailwind: {
 				command: `npx tailwindcss build assets/css/tailwind/style.css -o assets/build/css/tailwind.min.css --silent`,
 			},
-			npm_wordpress: {
-				command: `npm run build:wp`,
-			},
+			npm_typescript: { command: 'npx tsc' },
+			npm_webpack: { command: 'npx webpack' },
+			npm_wordpress: { command: `npm run build:wp` },
 			original_assets: { command: `node originalassets.js` },
 			sass: {
 				command: () => {
@@ -61,27 +77,28 @@ module.exports = function (grunt) {
 			},
 		},
 
+		/** Uglify - JS Compressor and minifier */
+		uglify: {
+			options: { sourceMap: false },
+			build: {
+				files: (() => {
+					let assets = {};
+					scriptConfig.map(s => {
+						let path = `${s.output.path}/${s.output.filename}`;
+						assets[path] = [path];
+					});
+					return assets;
+				})()
+			},
+		},
+
 		/** Configure watch task */
 		watch: {
 			options: {
 				livereload: false,
 			},
-			blocks: (() => { /** WordPress Blocks and Shortcodes (React JS) */
-				let watcher = { files: [] };
-				componentConfig.map((b) => {
-					let FilesTasks = {
-						files: [
-							`blocks/${b.name}/**/*.js`,
-							`blocks/${b.name}/**/*.jsx`,
-						],
-						tasks: [`build-shortcode-${b.name}`, 'build-css']
-					};
-					watcher[`component_${b.name}`] = FilesTasks;
-				});
-				return watcher;
-			})(),
-			components: (() => { /** Svelte Components */
-				let watcher = { files: [] };
+			...(() => { /** Svelte Components */
+			let watcher = {};
 				componentConfig.map((c) => {
 					let FilesTasks = {
 						files: [
@@ -94,12 +111,31 @@ module.exports = function (grunt) {
 				});
 				return watcher;
 			})(),
-			js: {
-				files: [
-					'assets/js/**/*.js'
-				],
-				tasks: ['build-js'],
-			},
+			...(() => { /** Typescript Library */
+			let watcher = {};
+				scriptConfig.map(s => {
+					let FilesTasks = {
+						files: [ `assets/ts/${s.name}/**/*.ts` ],
+						tasks: [`build-script-${s.name}`]
+					};
+					watcher[`script-${s.name}`] = FilesTasks;
+				});
+				return watcher;
+			})(),
+			...(() => { /** WordPress Blocks and Shortcodes (React JS) */
+			let watcher = {};
+				blockConfig.map((b) => {
+					let FilesTasks = {
+						files: [
+							`blocks/${b.name}/**/*.js`,
+							`blocks/${b.name}/**/*.jsx`,
+						],
+						tasks: [`build-block-${b.name}`, 'build-css']
+					};
+					watcher.files[`component_${b.name}`] = FilesTasks;
+				});
+				return watcher;
+			})(),
 			css: {
 				files: [
 					'assets/css/**/*.scss',
@@ -115,19 +151,34 @@ module.exports = function (grunt) {
 
 	/** Load Plugin */
 	grunt.loadNpmTasks('grunt-shell')
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
 	grunt.loadNpmTasks('grunt-contrib-watch')
 	grunt.loadNpmTasks('grunt-contrib-cssmin')
 
-	/** Shortcodes */
-	let ShortcodeBuild = [];
+	/** Component */
+	let ComponentsBuild = [];
+	componentConfig.map((c) => {
+		ComponentsBuild.push(`build-component-${c}`);
+		grunt.registerTask(`build-component-${c}`, [ `shell:rollup_component_${c}` ]);
+	});
+
+	/** Typescripts using webpack */
+	let ScriptBuild = [];
+	scriptConfig.map(s => {
+		ScriptBuild.push(`build-script-${s.name}`);
+		grunt.registerTask(`build-script-${s.name}`, [ `shell:npm_webpack_${s.name}`, 'uglify' ]);
+	});
+
+	/** Blocks */
+	let BlocksBuild = [];
 	blockConfig.map((b) => {
-		ShortcodeBuild.push(`build-shortcode-${b.name}`)
-		grunt.registerTask(`build-shortcode-${b.name}`, [ `shell:build_shortcode_${b.name}` ]);
+		BlocksBuild.push(`build-block-${b.name}`)
+		grunt.registerTask(`build-block-${b.name}`, [ `shell:build_block_${b.name}` ]);
 	})
 
 	/** Register Tasks */
 	grunt.registerTask('build-css', [ 'shell:npm_tailwind', 'shell:sass', 'cssmin' ])
-	grunt.registerTask('build-js', ['shell:npm_wordpress', ...ShortcodeBuild])
+	grunt.registerTask('build-js', ['shell:npm_webpack', 'shell:npm_wordpress', ...ComponentsBuild, ...ScriptBuild, ...BlocksBuild])
 	grunt.registerTask('build', ['build-css', 'build-js'])
 	grunt.registerTask('default', ['build', 'shell:original_assets'])
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,40 @@
 const path = require('path');
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
 let blocksConfig = require( path.resolve(__dirname, 'blocks/blocks.json') );
+let scriptsConfig = require( path.resolve(__dirname, 'assets/ts/scripts.json') );
+
+/** Generate TypeScript Config */
+scriptsConfig = scriptsConfig.map((c) => {
+	let config = {
+		...c,
+		mode: 'development',
+		devtool: 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.ts?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+			]
+		},
+		resolve: {
+			extensions: ['.ts']
+		}
+	};
+	config.output.path = path.resolve(__dirname, config.output.path);
+	return config;
+});
 
 /** Generate Blocks Config */
 blocksConfig = blocksConfig.map((c) => {
-    let config = {
+	let config = {
 		...defaultConfig,
-        ...c,
-    };
-    config.output.path = path.resolve(__dirname, config.output.path);
-    return config;
+		...c,
+	};
+	config.output.path = path.resolve(__dirname, config.output.path);
+	return config;
 });
-module.exports = [ ...blocksConfig ];
+
+// Export Module
+module.exports = [ ...scriptsConfig, ...blocksConfig ];
